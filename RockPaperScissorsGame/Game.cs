596bee3353594fc1798ac3db92a359208a343f4c
kadirermaya <kadir.ermaya@gmail.com>
2020-06-11 13:41:46 -0700

    Created a method displays Gestures

--- RockPaperScissorsGame/Game.cs
+++ RockPaperScissorsGame/Game.cs
@@ -6,10 +6,36 @@ using System.Threading.Tasks;
 
 namespace RockPaperScissorsGame
 {
-    class Game
+    public class Game
     {
         //Member Variables (HAS A)
+
+        public List<string> gestures;
+
         //Constructor (SPAWNER)
+        public Game()
+        {
+            
+        }
         //Member Methods (CAN DO)
+
+        public void AddGestures()
+        {
+            gestures = new List<string>();
+            gestures.Add("rock");
+            gestures.Add("paper");
+            gestures.Add("scissors");
+            gestures.Add("spock");
+            gestures.Add("lizard");
+        }
+
+        public void DisplayGestures()
+        {
+            Console.WriteLine("Please choose a gesture!");
+            Console.WriteLine($"0: {gestures[0]} 1: {gestures[1]} 2: {gestures[2]} 3: {gestures[3]} 4: {gestures[4]}");
+            
+        }
+
+
     }
 }
