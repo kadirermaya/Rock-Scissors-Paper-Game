bbd57ce95fa64c8a931a1fc762247cb24e2a0839
kadirermaya <kadir.ermaya@gmail.com>
2020-06-11 13:11:07 -0700

    Classes Created

---
+++ RockPaperScissorsGame/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissorsGame
+{
+    class Player
+    {
+        //Member Variables (HAS A)
+        //Constructor (SPAWNER)
+        //Member Methods (CAN DO)
+    }
+}
