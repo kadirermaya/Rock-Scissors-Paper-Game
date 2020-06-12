11b5329db55a03c9758752cdd40ca41959c7b6d8
kadirermaya <kadir.ermaya@gmail.com>
2020-06-12 15:41:38 -0700

    validation added into the ChoosePlayerName method

--- RockPaperScissorsGame/Human.cs
+++ RockPaperScissorsGame/Human.cs
@@ -16,12 +16,21 @@ namespace RockPaperScissorsGame
             type = "human";
         }
         //Constructor (SPAWNER)
+
         //Member Methods (CAN DO)
+        
+        // players can choose their names and validates
+        
         public override void ChoosePlayerName()
         {
             Console.WriteLine($"\nGreat! Lets pick a name for {name}");
             Console.WriteLine($"Type your player name and click ENTER!");
             name = Console.ReadLine();
+            while (string.IsNullOrEmpty(name))          // validation added
+            {
+                Console.WriteLine($"Name cannot be EMPTY!");
+                name = Console.ReadLine();
+            }
             Console.WriteLine($"You succesfuly changed it. Your player name is {name} now!");
             Console.WriteLine();
         }
@@ -31,7 +40,7 @@ namespace RockPaperScissorsGame
             AddGestures();
                 for (int i = 0; i < gestures.Count; i++)
                 {
-                    Console.Write($"{i + 1}:{gestures[i]} ");
+                    Console.Write($"{i + 1}:{gestures[i]} ");                 
                 }
             Console.WriteLine($"\n{name} choose your Gesture!");
                 gesture = Console.ReadLine();
